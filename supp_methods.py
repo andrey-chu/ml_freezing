ba816a62c3343c17b0dae8e95e5222ef5446f0d3
Andrey Chuhutin <end.product@gmail.com>
2019-12-25 02:29:36 +0100

    corrected error in freezing point estimation in seglearn file

--- supp_methods.py
+++ supp_methods.py
@@ -188,14 +188,18 @@ def find_freezing_by_frozen(predicted_array, wells_set):
         array_i=predicted_array[:,i]
         reversed_array = array_i[::-1]
         if np.sum(array_i==1)==0:
+        # if there is no 1's in array
             itera = 0
             j = reversed_array[itera]
+            # let's go from the end
             if np.sum(reversed_array==2)==0:
-                
+                # if there are no 2's in aray 
+                # we go back until we encounter anything which is not 3 or nan
                 while (j==3 or (np.isnan(j))):
                     itera +=1
                     j=reversed_array[itera]
             else:
+                # if there are 2's in array we go back until the first 2
                 while (j!=2):
                     itera +=1
                     j=reversed_array[itera]
@@ -203,6 +207,45 @@ def find_freezing_by_frozen(predicted_array, wells_set):
             predicted_array[-itera,i] =1
             freezing_points[0,ii] = length_array-itera
         else:
+            # if there are 1's than the first one is the freezing point
+            freezing_points[0,ii] = np.min(np.nonzero(array_i==1)[0])
+        ii+=1
+    return (freezing_points, predicted_array)
+
+def seg_find_freezing_by_frozen(predicted_array1):
+    import numpy as np
+    predicted_array = predicted_array1.copy()
+    freezing_points = np.zeros((1,len(predicted_array)))
+    #import pdb; pdb.set_trace()
+    ii = 0
+    for i in range(len(predicted_array)):
+        length_array = predicted_array[i].shape[0]
+        array_i=predicted_array[i]
+        reversed_array = array_i[::-1]
+        if np.sum(array_i==1)==0:
+        # if there is no 1's in array
+            itera = 0
+            j = reversed_array[itera]
+            while (j!=3 and not(np.isnan(j))):
+                itera +=1
+                j=reversed_array[itera]
+            # let's go from the end
+            if np.sum(reversed_array==2)==0:
+                # if there are no 2's in aray 
+                # we go back until we encounter anything which is not 3 or nan
+                while (j==3 or (np.isnan(j))):
+                    itera +=1
+                    j=reversed_array[itera]
+            else:
+                # if there are 2's in array we go back until the first 2
+                while (j!=2):
+                    itera +=1
+                    j=reversed_array[itera]
+            #reversed_array[itera] =1
+            predicted_array[i][-itera] =1
+            freezing_points[0,ii] = length_array-itera
+        else:
+            # if there are 1's than the first one is the freezing point
             freezing_points[0,ii] = np.min(np.nonzero(array_i==1)[0])
         ii+=1
     return (freezing_points, predicted_array)
